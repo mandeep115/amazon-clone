aa64c543334a3602adc6593d86daedf9604a0ec0
mandeep115 <mandeepab51@gmail.com>
2020-08-26 01:58:58 +0530

    Almost finished HomePage with Header and products next React content API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Header from './Header'
+import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className="app">
         <Switch>
           <Route path="/checkout">
+            <Header />
             <h1>Checkout</h1>
           </Route>
           <Route path="/login">
@@ -17,7 +19,7 @@ function App() {
           </Route>
           <Route path="/">
             <Header />
-            <h1>HOME PAGE!!!</h1>
+            <Home />
           </Route>
         </Switch>
       </div>
