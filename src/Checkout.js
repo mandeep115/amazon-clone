5b6aef5fa900bfd8a9edc75f5d06d0fc226a817c
mandeep115 <mandeepab51@gmail.com>
2020-09-01 00:13:28 +0530

    Finished Checkout page

---
+++ src/Checkout.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Checkout.css";
+import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
+import Subtotal from "./Subtotal";
+
+function Checkout() {
+  const [{ basket }] = useStateValue();
+
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
+          src="https://m.media-amazon.com/images/G/31/AMS/IN/970X250-_desktop_banner.jpg"
+          alt=""
+          className="checkout__ad"
+        />
+        {basket.length === 0 ? (
+          <div>
+            <h2>Empty</h2>
+          </div>
+        ) : (
+          <div>
+            <h1 className="checkout__title">Your basket</h1>
+            {basket.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+      <div className="checkout__right">
+        <Subtotal />
+      </div>
+    </div>
+  );
+}
+
+export default Checkout;
--- src/Product.js
+++ src/Product.js
@@ -1,6 +1,23 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "./StateProvider";
+
 function Product({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id,
+        title,
+        image,
+        price,
+        rating,
+      },
+    });
+  };
+
   return (
     <div className="product">
       <div className="product__info">
@@ -18,7 +35,7 @@ function Product({ id, title, image, price, rating }) {
         </div>
       </div>
       <img src={image} alt="" />
-      <button>Add To Basket</button>
+      <button onClick={addToBasket}>Add To Basket</button>
     </div>
   );
 }
