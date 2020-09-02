e300bef07b48363475d5ffad87f81314b385e802
mandeep115 <mandeepab51@gmail.com>
2020-09-02 16:07:21 +0530

    Finished subtotal section in checkout

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,30 +1,30 @@
 import React from "react";
 import "./Subtotal.css";
-//  import CurrencyFormat from "react-currency-format";
+import CurrencyFormat, { NumberFormat } from "react-currency-format";
 import { useStateValue } from "./StateProvider";
 
 function Subtotal() {
   const [{ basket }] = useStateValue();
   return (
     <div className="subtotal">
-      {/* <CurrencyFormat
+      <CurrencyFormat
+        decimalScale={2}
+        value={0}
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"₹"}
         renderText={(value) => (
-      <> */}
-      <div>
-        <p>
-          Subtotal ({basket.length} items): <strong>{`₹ 0`}</strong>
-        </p>
-        <small className="subtotal__gift">
-          <input type="checkbox" /> This order contains a gift
-        </small>
-      </div>
-      {/* </>
-       )}  decimalScale={2}
-       value={0}
-       displayText={"text"}
-       thousandSeprater={true}
-       prefix={"₹"}
-       /> */}
+          <div>
+            <p>
+              Subtotal ({basket.length} items): <strong>{`${value}`}</strong>
+            </p>
+            <small className="subtotal__gift">
+              <input type="checkbox" /> This order contains a gift
+            </small>
+          </div>
+        )}
+      />
+
       <button>Proceed to Checkout</button>
     </div>
   );
