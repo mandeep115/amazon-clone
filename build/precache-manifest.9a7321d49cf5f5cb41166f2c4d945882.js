21447481c549c9a265d7e6c097790099b83f1e68
mandeep115 <mandeepab51@gmail.com>
2020-09-10 01:10:56 +0530

    Added Production version

---
+++ build/precache-manifest.9a7321d49cf5f5cb41166f2c4d945882.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73475ce2bc5aec330e71dccc891b7f4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06bf9d39dbb54c40d4fc",
+    "url": "/static/css/main.b79a6125.chunk.css"
+  },
+  {
+    "revision": "a04d2b7726030826c095",
+    "url": "/static/js/2.d581800f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d581800f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06bf9d39dbb54c40d4fc",
+    "url": "/static/js/main.4873860f.chunk.js"
+  },
+  {
+    "revision": "92a146e5ab9e0f9e785b",
+    "url": "/static/js/runtime-main.90140090.js"
+  }
+]);
